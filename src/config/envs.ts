8490606e10c6a37f1ecf4400078747c556ae20b0
Nicolay <nicolaymorenoherrera@gmail.com>
2024-10-17 18:41:19 -0500

    chore: Add NATS microservices configuration

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -5,24 +5,35 @@ import * as joi from 'joi';
 // Define an interface for type safety
 interface EnvVars {
     PORT: number;
+
     STRIPE_SECRET: string;
     STRIPE_SUCCESS_URL: string;
     STRIPE_CANCEL_URL: string;
     STRIPE_ENDPOINT_SECRET: string;
+
+    NATS_SERVERS: string[];
 }
 
 // Define the validation schema for environment variables
 const envsSchema = joi.object({
     PORT: joi.number().required(),
+
     STRIPE_SECRET: joi.string().required(),
     STRIPE_SUCCESS_URL: joi.string().required(),
     STRIPE_CANCEL_URL: joi.string().required(),
     STRIPE_ENDPOINT_SECRET: joi.string().required(),
+
+    NATS_SERVERS: joi.array().items( joi.string() ).required(),
 })
-    .unknown(true);
+.unknown(true);
 
 // Validate the environment variables against the schema
-const { error, value } = envsSchema.validate(process.env);
+const { error, value } = envsSchema.validate({
+
+    ...process.env,
+    NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
+
+});
 
 // Throw an error if validation fails
 if (error) {
@@ -35,8 +46,11 @@ const envVars: EnvVars = value;
 // Export the validated and typed environment variables
 export const envs = {
     port: envVars.PORT,
+
     stripeSecret: envVars.STRIPE_SECRET,
     stripeSuccessUrl: envVars.STRIPE_SUCCESS_URL,
     stripeCancelUrl: envVars.STRIPE_CANCEL_URL,
     stripeEndpointSecret: envVars.STRIPE_ENDPOINT_SECRET,
+
+    natsServers: envVars.NATS_SERVERS,
 }
